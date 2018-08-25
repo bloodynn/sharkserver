9b83b7c99ba1732641234a5e7aa0ac33455829c7
lakre <akre.lionel@free.fr>
2018-08-25 05:05:35 +0200

    partie serveur node express

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+var session = require('express-session')
+var bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(session({
+  secret: 'keyboard cat'
+}))
+
+let inputList = [];
 
 app.get('/albums',(request, response) => {
   const MongoClient = require('mongodb').MongoClient;
@@ -16,6 +25,27 @@ app.get('/albums',(request, response) => {
         albums.close();
       }
   })().catch(err => console.error(err)); 
+})
+
+app.post('/session/inputs',(request,response) => {
+
+  console.log(request.body)
+  if(inputList.length === 0){
+    this.inputList=[]
+  }
+  this.inputList.push(request.body.input)
+  response.set({"Access-Control-Allow-Origin": "*"})
+  response.send("reussi")
+
+})
+
+app.get('/session/inputs',(request,response) => {
+  const res = this.inputList;
+  console.log("get:",res)
+  this.inputList = [];
+  response.set({"Access-Control-Allow-Origin": "*"})
+  response.send(res)
+
 })
 
 app.listen(port, (err) => {
