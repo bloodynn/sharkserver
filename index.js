1e16b344a86b0e567de38dd079afa6e592844d72
lakre <akre.lionel@free.fr>
2018-08-29 07:59:30 +0200

    sauvegarde de session ok

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ var bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
-const ON_WAIT = "waiting"
-
-const ON_PLAY = "playing"
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(session({
-  secret: 'keyboard cat'
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    httpOnly:false,
+    expires:false
+}
 }))
 
 /*variable globale pour l'input
 * à l'avenir, ce sera une variable de session
 */
 global.input = [];
+
+const ON_WAIT = "waiting"
+const ON_PLAY = "playing"
 
 global.game = {
   state : ON_WAIT,
@@ -26,17 +32,17 @@ global.game = {
 /*endpoint enregistrement du joueur*/
 app.post('/session/player',(request,response) => {
   
-  required.session.name = response.body.name;
+  request.session.name = response.body.name;
   if (game.player1 !== null){
     game.player1 = response.body.name;
-    required.session.playerNumber = 1;
+    request.session.playerNumber = 1;
   }
   else{
     game.player2 = response.body.name;
-    required.session.playerNumber = 2;
+    request.session.playerNumber = 2;
   }
   response.set({"Access-Control-Allow-Origin": "*"})
-  response.send("Joueur numero:",required.session.playerNumber);
+  response.send("Joueur numero:",request.session.playerNumber);
 })
 
 /*endpoit de la demande d'état de la partie*/
@@ -53,23 +59,26 @@ app.get('/session/state',(request,response) => {
 
 /* endpoint de réception de la direction */
 app.post('/session/inputs',(request,response) => {
-  if(players <= 2){
-    if(!required.session.playerNumber){
+  if(game.players < 2){
+    if(!request.session.playerNumber){
+      console.log("player",request.session.playerNumber)
       game.players++;
-      required.session.playerNumber = players;
+      request.session.playerNumber = game.players;
+      console.log("here comes a new challenger")
+      console.log(request.session)
     }
-    input[required.session.playerNumber-1]=request.body;
+    input[request.session.playerNumber-1]=request.body;
+    request.session.save()
+    response.set('Access-Control-Allow-Credentials', 'true');
     response.set({"Access-Control-Allow-Origin": "*"})
     response.send("reussi")
     console.log("post:",request.body)
   }
   else{
+    request.session.save()
     response.set({"Access-Control-Allow-Origin": "*"})
     response.send("plus d'espace joueur disponibles")
   }
-  
-  
-
 })
 
 /* endpoint de la demande de la direction */
